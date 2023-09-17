e2275b0d9c2433cc276971d63f3b3e1c576cad1f
Oliver Marler <103157332+rocklandceo@users.noreply.github.com>
2023-09-16 22:09:20 -0700

    new commit

--- html2md.py
+++ html2md.py
@@ -4,38 +4,15 @@ import os
 import re
 from bs4 import BeautifulSoup
 
-def old_html_to_markdown(url):
-    # Fetch HTML content from the URL
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for HTTP errors
-
-    # Use BeautifulSoup to extract the title from the HTML content
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title_element = soup.find('title')  # Assuming the title is within <title> tags
-    title = title_element.text if title_element else "default-title"
-    
-    # Format the title to be used as a filename
-    formatted_title = re.sub('[^a-zA-Z0-9\s]', '', title).replace(' ', '-').lower()
-
-    # Convert HTML to Markdown
-    h = html2text.HTML2Text()
-    h.ignore_links = False
-    markdown_content = h.handle(response.text)
-
-    return formatted_title, markdown_content
-
-
 import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 def html_to_markdown(url):
-    # Automatically download and install ChromeDriver
-    chromedriver_autoinstaller.install()
 
-    # Set up the Selenium driver
+    # Automatically download and install Chrome DriverSet up the Selenium driver
     chromedriver_autoinstaller.install()
-    service = Service(chromedriver_autoinstaller.driver_filename)
+    service = Service()
     driver = webdriver.Chrome(service=service)
     
     # Fetch HTML content from the URL using Selenium
