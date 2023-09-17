3b53d9ab27f46a5e779e1d89e7d950f7e92b639a
Oliver Marler <103157332+rocklandceo@users.noreply.github.com>
2023-09-16 20:56:29 -0700

    Reverted html2md.py and removed parser submodule

--- html2md.py
+++ html2md.py
@@ -2,47 +2,42 @@ import html2text
 import requests
 import os
 import re
-import subprocess
-import json
 from bs4 import BeautifulSoup
 
-def parse_with_postlight_parser(url):
-    # Define the path to the cli.js script inside the parser submodule
-    cli_path = './parser/cli.js'
+def html_to_markdown(url):
+    # Fetch HTML content from the URL
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+
+    # Use BeautifulSoup to extract the title from the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title_element = soup.find('title')  # Assuming the title is within <title> tags
+    title = title_element.text if title_element else "default-title"
     
-    # Use subprocess to run the script and capture the output
-    result = subprocess.check_output(['node', cli_path, url])
-    
-    # Parse the JSON output to get the parsed content
-    parsed_data = json.loads(result)
-    return parsed_data.get('content', '')
+    # Format the title to be used as a filename
+    formatted_title = re.sub('[^a-zA-Z0-9\s]', '', title).replace(' ', '-').lower()
+
+    # Convert HTML to Markdown
+    h = html2text.HTML2Text()
+    h.ignore_links = False
+    markdown_content = h.handle(response.text)
+
+    return formatted_title, markdown_content
 
 if __name__ == "__main__":
     url = input("Enter the URL: ")
-    
-    # Parse the content using Postlight Parser
-    html_content = parse_with_postlight_parser(url)
-    
-    # Convert the HTML content to Markdown
-    h = html2text.HTML2Text()
-    h.ignore_links = False
-    markdown_content = h.handle(html_content)
-    
-    # Extract title for filename using BeautifulSoup
-    soup = BeautifulSoup(html_content, 'html.parser')
-    title_element = soup.find('title')
-    title = title_element.text if title_element else "default-title"
-    formatted_title = re.sub('[^a-zA-Z0-9\s]', '', title).replace(' ', '-').lower()
-    
-    # Define the path for the markdown file
+    formatted_title, markdown_content = html_to_markdown(url)
+
+    # Ensure 'converted-files' directory exists or create it
     output_directory = "converted-files"
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
+
+    # Define the path for the markdown file
     markdown_output_path = os.path.join(output_directory, f"{formatted_title}.md")
-    
+
     # Save the Markdown content
     with open(markdown_output_path, "w", encoding='utf-8') as f:
         f.write(markdown_content)
 
-    print(f"Content from the URL has been parsed and saved to '{markdown_output_path}'.")
-
+    print(f"HTML content from the URL has been converted and saved to '{markdown_output_path}'.")
